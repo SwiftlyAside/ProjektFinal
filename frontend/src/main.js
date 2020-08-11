3fa11248ede736bfac55b59973c6bd37735d9d3f
Swiftly <ivecis@live.co.kr>
2020-08-11 17:37:16 +0900

    init.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue';
+import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import vuetify from './plugins/vuetify';
 
-createApp(App).use(router).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  transpileDependencies: [
+    'vuetify',
+  ],
   outputDir: path.resolve(__dirname, '../src/main/resources/static'),
 };
