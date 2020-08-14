0f74b7eae220edbfd47ccbf41e740414c26b9f87
Swiftly <ivecis@live.co.kr>
2020-08-14 12:10:26 +0900

    Modify something
    의존성 추가 (vee-validate)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import './plugins/axios';
 import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
-import App from './App1.vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
 
