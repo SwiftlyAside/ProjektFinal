b20819af45daa5a6ec05cfcda29249e0c84874a3
Swiftly <ivecis@live.co.kr>
2020-08-14 17:57:39 +0900

    Add Product.vue Modify something
    상품등록 컴포넌트 추가
    axios 전역변수화
    그 외 많이 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import './plugins/axios';
+import Axios from 'axios';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
 
+Vue.prototype.$http = Axios;
 Vue.config.productionTip = false;
 
 new Vue({
