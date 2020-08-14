fb96aa7a1c0dc1e931b4b27540556fce4eede190
Swiftly <ivecis@live.co.kr>
2020-08-14 11:13:22 +0900

    Modify something
    Merge 후 작업

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import './plugins/axios';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
